194b0dc6868999e0010d7d82147ab1f9951071f9
ltt19921124 <ltt19921124@163.com>
2017-02-14 00:12:18 +0800

    k
    控件

--- UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.uiwidgettest;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.media.Image;
+import android.preference.DialogPreference;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
 
     private EditText editText;
+
     private ImageView imageView;
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         Button button = (Button) findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.edit_text);
         imageView = (ImageView) findViewById(R.id.image_view);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
         button.setOnClickListener(this);
     }
 
@@ -28,7 +37,12 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.button:
-                imageView.setImageResource(R.drawable.img_2);
+                ProgressDialog progressDialog = new ProgressDialog(
+                    MainActivity.this);
+                progressDialog.setTitle("This is ProgressDialog");
+                progressDialog.setMessage("Loading....");
+                progressDialog.setCancelable(true);
+                progressDialog.show();
                 break;
             default:
                 break;
