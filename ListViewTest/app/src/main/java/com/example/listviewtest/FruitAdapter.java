6f0cbd29f00f1e1aabc385665698426f2fe3c96f
ltt19921124 <ltt19921124@163.com>
2017-02-15 09:08:47 +0800

    k
    system

---
+++ ListViewTest/app/src/main/java/com/example/listviewtest/FruitAdapter.java
@@ -0,0 +1,55 @@
+package com.example.listviewtest;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+import java.util.List;
+
+/**
+ * Created by Tian Lu on 2017/2/14.
+ */
+
+public class FruitAdapter extends ArrayAdapter<Fruit> {
+
+    private int resourceId;
+
+    public FruitAdapter(Context context, int textViewResourceId,
+                        List<Fruit> objects){
+        super(context,textViewResourceId,objects);
+        resourceId = textViewResourceId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Fruit fruit = getItem(position);
+        View view;
+        ViewHolder viewHolder;
+        if (convertView == null){
+            view = LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
+            viewHolder = new ViewHolder();
+            viewHolder.fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            viewHolder.fruitName = (TextView) view.findViewById(R.id.fruit_name);
+            view.setTag(viewHolder);
+        }else{
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
+        ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+        TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
+        fruitImage.setImageResource(fruit.getImageId());
+        fruitName.setText(fruit.getName());
+        return view;
+    }
+
+    class ViewHolder{
+        ImageView fruitImage;
+        TextView fruitName;
+    }
+}
---
+++ RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/Fruit.java
@@ -0,0 +1,25 @@
+package com.example.recyclerviewtest;
+
+/**
+ * Created by Tian Lu on 2017/2/14.
+ */
+
+public class Fruit {
+
+    private String name;
+
+    private int imageId;
+
+    public Fruit(String name, int imageId){
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getImageId(){
+        return imageId;
+    }
+}
