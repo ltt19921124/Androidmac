a4d24da974de169841e05dd9b026257103425ddd
ltt19921124 <ltt19921124@163.com>
2017-03-18 22:10:07 +0800

    k
    k

--- NotificationTest/app/src/main/java/com/example/notificationtest/MainActivity.java
+++ NotificationTest/app/src/main/java/com/example/notificationtest/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.notificationtest;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.graphics.BitmapFactory;
-import android.provider.Settings;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
-import android.view.View;
-import android.widget.Button;
+        import android.app.Notification;
+        import android.app.NotificationManager;
+        import android.app.PendingIntent;
+        import android.content.Intent;
+        import android.graphics.BitmapFactory;
+        import android.provider.Settings;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.support.v7.app.NotificationCompat;
+        import android.view.View;
+        import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -24,6 +26,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.send_notice:
+                Intent intent = new Intent(this,NotificationActivity.class);
+                PendingIntent pi = PendingIntent.getActivity(this,0,intent,0);
                 NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
                 Notification notification = new NotificationCompat.Builder(this)
                         .setContentTitle("This is content title")
@@ -32,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(),
                                 R.mipmap.ic_launcher))
+                        .setContentIntent(pi)
                         .build();
                 manager.notify(1,notification);
                 break;
