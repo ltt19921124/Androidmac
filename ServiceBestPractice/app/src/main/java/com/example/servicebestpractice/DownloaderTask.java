ac03011edf664b7af544dcc87fb25f4763a3e5ac
ltt19921124 <ltt19921124@163.com>
2017-04-09 02:47:14 +0800

    k
    k

--- ServiceBestPractice/app/src/main/java/com/example/servicebestpractice/DownloaderTask.java
+++ ServiceBestPractice/app/src/main/java/com/example/servicebestpractice/DownloaderTask.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.os.Environment;
 import android.os.ParcelUuid;
 import android.sax.StartElementListener;
+import android.webkit.DownloadListener;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,21 +20,23 @@ import okhttp3.Response;
  * Created by Tian Lu on 2017/3/31.
  */
 
-public class DownloaderTask extends AsyncTask<String,Integer,Integer> {
+public class DownloadTask extends AsyncTask<String, Integer, Integer> {
+
     public static final int TYPE_SUCCESS = 0;
-
     public static final int TYPE_FAILED = 1;
-
     public static final int TYPE_PAUSED = 2;
+    public static final int TYPE_CANCELED = 3;
 
-    public static final int TYPE_CANCLED = 3;
 
-    private DownloaderListener listener;
-    private boolean isCancled = false;
+    private DownloadListener listener;
+
+    private boolean isCanceled = false;
+
     private boolean isPaused = false;
+
     private int lastProgress;
 
-    public DownloaderTask(DownloaderListener listener){
+    public DownloadTask(DownloadListener listener) {
         this.listener = listener;
     }
 
@@ -42,62 +45,66 @@ public class DownloaderTask extends AsyncTask<String,Integer,Integer> {
         InputStream is = null;
         RandomAccessFile savedFile = null;
         File file = null;
-        try{
-            long downloadedLength = 0;//记录已下载的文件长度
-            String downloaderUrl = params[0];
-            String fileName = downloaderUrl.substring(downloaderUrl.lastIndexOf("/"));
-            String directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-                    .getPath();
+        try {
+            long downloadedLength = 0; // 记录已下载的文件长度
+            String downloadUrl = params[0];
+            String fileName = downloadUrl.substring(downloadUrl.lastIndexOf("/"));
+            String directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
             file = new File(directory + fileName);
             if (file.exists()) {
                 downloadedLength = file.length();
             }
-            long contentLength = getContentLength(downloaderUrl);
-            if (contentLength == 0){
+            long contentLength = getContentLength(downloadUrl);
+            if (contentLength == 0) {
                 return TYPE_FAILED;
-            }else if (contentLength == downloadedLength){
-                // 已下载字节和文件总字节相等，说明已经下载完了
+            } else if (contentLength == downloadedLength) {
+                // 已下载字节和文件总字节相等，说明已经下载完成了
                 return TYPE_SUCCESS;
             }
             OkHttpClient client = new OkHttpClient();
-            Response request = new Response.Builder()
-                    .addHeader("RANGE","bytes=" + downloadedLength + "-")
-                    .url(downloaderUrl)
+            Request request = new Request.Builder()
+                    // 断点下载，指定从哪个字节开始下载
+                    .addHeader("RANGE", "bytes=" + downloadedLength + "-")
+                    .url(downloadUrl)
                     .build();
             Response response = client.newCall(request).execute();
             if (response != null) {
                 is = response.body().byteStream();
-                savedFile = new RandomAccessFile(file,"rw");
-                savedFile.seek(downloadedLength);
+                savedFile = new RandomAccessFile(file, "rw");
+                savedFile.seek(downloadedLength); // 跳过已下载的字节
                 byte[] b = new byte[1024];
                 int total = 0;
                 int len;
-                while ((len = is.read(b)) != -1){
-                    if (isCancled) {
-                        return TYPE_CANCLED;
-                    } else if (isPaused) {
+                while ((len = is.read(b)) != -1) {
+                    if (isCanceled) {
+                        return TYPE_CANCELED;
+                    } else if(isPaused) {
                         return TYPE_PAUSED;
                     } else {
                         total += len;
-                        savedFile.write(b,0,len);
+                        savedFile.write(b, 0, len);
+                        // 计算已下载的百分比
                         int progress = (int) ((total + downloadedLength) * 100 / contentLength);
                         publishProgress(progress);
-                     }
+                    }
                 }
                 response.body().close();
                 return TYPE_SUCCESS;
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
                 if (is != null) {
                     is.close();
                 }
-                if (savedFile != null){
+                if (savedFile != null) {
+                    savedFile.close();
+                }
+                if (isCanceled && file != null) {
                     file.delete();
                 }
-            } catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
@@ -107,7 +114,7 @@ public class DownloaderTask extends AsyncTask<String,Integer,Integer> {
     @Override
     protected void onProgressUpdate(Integer... values) {
         int progress = values[0];
-        if (progress > lastProgress){
+        if (progress > lastProgress) {
             listener.onProgress(progress);
             lastProgress = progress;
         }
@@ -125,17 +132,20 @@ public class DownloaderTask extends AsyncTask<String,Integer,Integer> {
             case TYPE_PAUSED:
                 listener.onPaused();
                 break;
-            case TYPE_CANCLED:
-                listener.onCancled();
+            case TYPE_CANCELED:
+                listener.onCanceled();
             default:
                 break;
         }
     }
+
     public void pauseDownload() {
         isPaused = true;
     }
+
+
     public void cancelDownload() {
-        isCancled = true;
+        isCanceled = true;
     }
 
     private long getContentLength(String downloadUrl) throws IOException {
@@ -151,4 +161,4 @@ public class DownloaderTask extends AsyncTask<String,Integer,Integer> {
         }
         return 0;
     }
-}
+
