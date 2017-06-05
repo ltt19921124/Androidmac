c5ab27fe1ab641689aafb0de35ab91649465fe6f
ltt19921124 <ltt19921124@163.com>
2017-06-06 07:36:25 +0800

    k
    k

--- leanring/ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
+++ leanring/ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -1,15 +1,17 @@
 package com.example.activitytest;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class FirstActivity extends AppCompatActivity {
+public class FirstActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,30 +21,20 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
-                startActivity(intent);
+               SecondActivity.actionStart(FirstActivity.this,"data1","data2");
             }
+
+
+
+
+
+
         });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main,menu);
-        return true;
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("FirstActivity","OnRestart");
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.add_item:
-                Toast.makeText(this,"You clicked Add",Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.remove_item:
-                Toast.makeText(this,"You clicked Remove",Toast.LENGTH_SHORT).show();
-                break;
-            default:
-        }
-        return true;
-    }
-
 }
---
+++ leanring/ListViewTest/app/src/main/java/com/example/listviewtest/Fruit.java
@@ -0,0 +1,24 @@
+package com.example.listviewtest;
+
+/**
+ * Created by Tian Lu on 2017/6/5.
+ */
+
+public class Fruit {
+    private String name;
+
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
